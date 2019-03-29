c97cb2dcbbd3b2e70d93f441597ab290dda3caa5
shiyou <8533334@qq.com>
2019-03-29 10:55:38 +0800

    a1

---
+++ dist/js/common.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('.handle-item button').on('click', function () {
+        // 寻找元素
+        const $this = $(this);
+        const text = $this.text();
+        const attr = $this.attr('data-type');
+        const $top = $this.parents('.show-case-wrap');
+        const $case_main = $top.find('.case-main');
+        const $btns = $this.parent('.handle-item').find('button');
+
+        if($(this).attr('data-item')){
+            // 说明是作用在项目上
+            const $itemNum = $this.attr('data-item') - 1;
+            $case_main.find('.case-item').eq($itemNum).css(attr,text);
+        }else{
+            $case_main.css(attr,text);
+        }
+
+        // 激活状态
+        $btns.removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".show-case-wrap br").remove();
+
+    // 根据浏览器传递的type参数展示相应case = f1
+    let param = [];
+    let val = '';
+    const search = window.location.search;
+    if(window.location.search){
+        param = search.substring(1,search.length).split('&');
+    }
+
+    param.forEach((item)=>{
+        if(item.includes('case')){
+            val = item.split('=')[1];
+        }
+    });
+
+    if(val && val !== 'all'){
+        const arr = Array.from($('.case-box'));
+        arr.forEach((item)=>{
+            if($(item).attr('data-case') === val){
+                $(item).show();
+            }
+        });
+    }else{
+        $('.case-box').show();
+    }
+});
